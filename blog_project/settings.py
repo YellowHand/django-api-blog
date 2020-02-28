d3eb588114cd819f4b02ec7dc8a8cf7fb6555b27
Paweł Borkowski <ypawelborkowski@gmai.com>
2020-02-28 17:45:15 +0100

    user authentication

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,12 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd-party apps
-    'rest_framework',  # new
+    'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_auth',
+    'rest_auth.registration',
 
     # Local
-    'posts.apps.PostsConfig', # new
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,8 +133,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
+
